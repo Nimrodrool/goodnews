b820db19b722ba8c1ea0b828642c4c68654e3ea3
Nimrodrool <139579075+Nimrodrool@users.noreply.github.com>
2024-10-18 19:38:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-const swiper = new Swiper('.card-wrapper', {
-    // loop: true,
-    // observer: true,
-    // observeParents: true,
-    spaceBetween: 10,
-    slidesPerView: 4,
-    // freeMode: true,
-    // watchSlidesVisibility: true,
-    // watchSlidesProgress: true,
-  
-    // Pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    
-  
-  });
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal
+var img = document.getElementsByClassName("card-image");
+var modalImg = document.getElementById("modalImage");
+for (var i = 0; i < img.length; i++) {
+    img[i].onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+    }
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementById("closeModal");
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
